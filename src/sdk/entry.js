ddbf26c089065588c91e920a7fe6d66cae261b20
Brady <brown@Bradys-MacBook-Pro.local>
2021-08-20 12:49:04 -0400

    initial setup complete

---
+++ src/sdk/entry.js
@@ -0,0 +1,30 @@
+import * as Contentstack from 'contentstack';
+
+// Initialize the Contentstack Stack
+const Stack = Contentstack.Stack({
+  api_key: process.env.REACT_APP_APIKEY,
+  delivery_token: process.env.REACT_APP_DELIVERY_TOKEN,
+  environment: process.env.REACT_APP_ENVIRONMENT,
+  region: process.env.REACT_APP_REGION ? process.env.REACT_APP_REGION : "us",
+});
+
+export default {
+  getEntry(ctUid, locale) {
+    return new Promise((resolve, reject) => {
+      Stack.ContentType(ctUid)
+        .Query()
+        .language(locale)
+        .includeOwner()
+        .toJSON()
+        .find()
+        .then(
+          (result) => {
+            resolve(result)
+          },
+          (error) => {
+            reject(error)
+          }
+        )
+    })
+  },
+}
