fadacf80eaf988922f081a22e79d3719e6972c6e
Brady <brown@Bradys-MacBook-Pro.local>
2021-09-01 12:25:52 -0400

    deleted code comments

--- src/components/Slogan.jsx
+++ src/components/Slogan.jsx
@@ -3,26 +3,8 @@ import ReactHtmlParser from "react-html-parser";
 
 export const Slogan = (props) => {
 
-  // var stringToHTML = function (str) {
-  //   var dom = document.createElement('div');
-  //   dom.innerHTML = str;
-  //   return dom.body;
-  // };
-
-// const stringHtml = props.richText
-// const doc = new DOMParser().parseFromString(stringHtml, 'text/html');
-// let arr = [];
-
-// const showElms = parent => {
-//   const { children } = parent;
-//   if (children.length) Array.prototype.forEach.call(children, showElms);
-//   else console.log(parent.outerHTML);
-// }
-
   return (
     <div>
-      {/* {stringToHTML(props.richText)} */}
-      {/* {showElms(doc.body)} */}
       {ReactHtmlParser(props.richText)}
     </div>
   )
