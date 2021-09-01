ebbb84ee79c559b52af9bd341af95b9565bac75c
Brady <brown@Bradys-MacBook-Pro.local>
2021-09-01 12:23:52 -0400

    added support for RTE field

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import { Footer } from './components/Footer';
 import { Products } from './components/Products';
 import Nav from './components/Nav';
 import { ProductPage } from './components/ProductPage';
+import { Slogan } from './components/Slogan';
 
 class App extends React.Component {
   constructor(props) {
@@ -19,6 +20,7 @@ class App extends React.Component {
       header: undefined,
       footer: undefined,
       products: undefined,
+      slogan: undefined,
       navFilter: undefined,
     }  
 
@@ -36,6 +38,9 @@ class App extends React.Component {
       })
     }
 
+  // handleParse = (string) => {
+  //   return string.substring(0, string.length )
+  // }
 
   render() {
     if (this.state.loading === true) return <Loading text="Loading" />
@@ -50,6 +55,7 @@ class App extends React.Component {
              render={(props) => (
                <div>
                  <Nav products={this.state.products} handleNavClick={this.handleNavClick.bind(this)} handleFilterReset={this.handleFilterReset.bind(this)}/>
+                 <Slogan richText={this.state.slogan.rich_text_editor}/>
                  <Products products={this.state.products} filter={this.state.navFilter}/>
                </div>
              )}
@@ -71,10 +77,12 @@ class App extends React.Component {
     const header = await Stack.getEntry('header')
     const footer = await Stack.getEntry('footer')
     const products = await Stack.getEntry('product')
+    const slogan = await Stack.getEntry('company_status')
     this.setState({
       header: header[0][0],
       footer: footer[0][0],
-      products: products[0]
+      products: products[0],
+      slogan: slogan[0][0]
     })
 
     if (this.state.header === undefined) {
@@ -86,7 +94,7 @@ class App extends React.Component {
         loading: false
       })
     }
-    console.log('State: ', this.state)
+    console.log('slogan: ', this.state.slogan)
   }
 }
 
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export const Footer = (props) => {
   return (
-    <div>
+    <div className='footer-containter'>
       <p>{props.footer.contact_info.name}</p>
       <p>{props.footer.contact_info.address}</p>
       <p>{props.footer.contact_info.email}</p>
